583299bae906320dc9512215e0081ca58a65b3e1
Nathan Williams <nathan.williams@foundationdb.com>
2013-08-27 10:04:45 -0400

    Add a simple test class

---
+++ src/test/java/com/foundationdb/lucene/SimpleTest.java
@@ -0,0 +1,49 @@
+package com.foundationdb.lucene;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.Version;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest
+{
+    @Test
+    public void indexBasic() throws Exception {
+        StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+        // recreate the index on each execution
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        config.setCodec(new FDBCodec());
+        Directory luceneDir = new FDBDirectory();
+        try (IndexWriter writer = new IndexWriter(luceneDir, config)) {
+            writer.addDocument(Arrays.asList(
+                    new TextField("title", "The title of my first document", Store.YES),
+                    new TextField("content", "The content of the first document", Store.NO)));
+            
+            writer.addDocument(
+                    Arrays.asList(
+                            new TextField("title", "The title of the second document", Store.YES),
+                            new TextField("content", "And this is the content", Store.NO)
+                    )
+            );
+        }
+        assertDocumentsAreThere(luceneDir, 2);
+    }
+
+    private void assertDocumentsAreThere(Directory dir, int amount) throws IOException {
+        try (IndexReader reader = DirectoryReader.open(dir)) {
+            assertEquals(amount, reader.numDocs());
+        }
+    }
+}
