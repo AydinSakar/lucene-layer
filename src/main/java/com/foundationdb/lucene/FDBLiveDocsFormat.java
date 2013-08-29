a9cf23a014937cfdb2795ffefc8abc9dd4ad5a36
Nathan Williams <nathan.williams@foundationdb.com>
2013-08-29 12:17:53 -0400

    LivDocs implementation

--- src/main/java/com/foundationdb/lucene/FDBLiveDocsFormat.java
+++ src/main/java/com/foundationdb/lucene/FDBLiveDocsFormat.java
@@ -1,173 +1,131 @@
 package com.foundationdb.lucene;
 
+import com.foundationdb.KeyValue;
+import com.foundationdb.tuple.Tuple;
 import org.apache.lucene.codecs.LiveDocsFormat;
-import org.apache.lucene.index.IndexFileNames;
 import org.apache.lucene.index.SegmentInfoPerCommit;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.IOContext;
-import org.apache.lucene.store.IndexInput;
-import org.apache.lucene.store.IndexOutput;
-import org.apache.lucene.util.ArrayUtil;
 import org.apache.lucene.util.Bits;
-import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.CharsRef;
-import org.apache.lucene.util.IOUtils;
 import org.apache.lucene.util.MutableBits;
-import org.apache.lucene.util.StringHelper;
-import org.apache.lucene.util.UnicodeUtil;
 
 import java.io.IOException;
 import java.util.BitSet;
 import java.util.Collection;
 
+//
+// (livTuple) => (totalSize)
+// (livTuple, setBitIndex0) => []
+// (livTuple, setBitIndex1) => []
+// ...
+//
 public class FDBLiveDocsFormat extends LiveDocsFormat
 {
+    private static final String LIVEDOCS_EXTENSION = "liv";
+    private static final byte[] EMPTY_BYTES = new byte[0];
 
-    static final String LIVEDOCS_EXTENSION = "liv";
-
-    final static BytesRef SIZE = new BytesRef("size ");
-    final static BytesRef DOC = new BytesRef("  doc ");
-    final static BytesRef END = new BytesRef("END");
 
     @Override
-    public MutableBits newLiveDocs(int size) throws IOException {
-        return new FDBMutableBits(size);
+    public MutableBits newLiveDocs(int size) {
+        return new FDBBits(size);
     }
 
     @Override
-    public MutableBits newLiveDocs(Bits existing) throws IOException {
-        final FDBBits bits = (FDBBits) existing;
-        return new FDBMutableBits((BitSet) bits.bits.clone(), bits.size);
+    public MutableBits newLiveDocs(Bits existing) {
+        final FDBBits bits = (FDBBits)existing;
+        return new FDBBits(bits);
     }
 
     @Override
-    public Bits readLiveDocs(Directory dir, SegmentInfoPerCommit info, IOContext context) throws IOException {
-        assert info.hasDeletions();
-        BytesRef scratch = new BytesRef();
-        CharsRef scratchUTF16 = new CharsRef();
+    public Bits readLiveDocs(Directory directory, SegmentInfoPerCommit info, IOContext context) {
+        neverCalled();
 
-        String fileName = IndexFileNames.fileNameFromGeneration(info.info.name, LIVEDOCS_EXTENSION, info.getDelGen());
-        IndexInput in = null;
-        boolean success = false;
-        try {
-            in = dir.openInput(fileName, context);
+        assert info.hasDeletions() : "Has no deletions";
 
-            FDBUtil.readLine(in, scratch);
-            assert StringHelper.startsWith(scratch, SIZE);
-            int size = parseIntAt(scratch, SIZE.length, scratchUTF16);
+        FDBDirectory dir = FDBDirectory.unwrapFDBDirectory(directory);
+        Tuple livTuple = makeLivTuple(dir, info);
 
-            BitSet bits = new BitSet(size);
+        byte[] sizeBytes = dir.txn.get(livTuple.pack()).get();
+        assert sizeBytes != null : "No such livTuple";
 
-            FDBUtil.readLine(in, scratch);
-            while(!scratch.equals(END)) {
-                assert StringHelper.startsWith(scratch, DOC);
-                int docid = parseIntAt(scratch, DOC.length, scratchUTF16);
-                bits.set(docid);
-                FDBUtil.readLine(in, scratch);
-            }
-
-            success = true;
-            return new FDBBits(bits, size);
-        } finally {
-            if(success) {
-                IOUtils.close(in);
-            } else {
-                IOUtils.closeWhileHandlingException(in);
-            }
+        int totalSize = (int)Tuple.fromBytes(sizeBytes).getLong(0);
+        BitSet bits = new BitSet(totalSize);
+        for(KeyValue kv : dir.txn.getRange(livTuple.range())) {
+            int i = (int)Tuple.fromBytes(kv.getKey()).getLong(livTuple.size());
+            bits.set(i);
         }
-    }
-
-    private int parseIntAt(BytesRef bytes, int offset, CharsRef scratch) {
-        UnicodeUtil.UTF8toUTF16(bytes.bytes, bytes.offset + offset, bytes.length - offset, scratch);
-        return ArrayUtil.parseInt(scratch.chars, 0, scratch.length);
+        return new FDBBits(bits, totalSize);
     }
 
     @Override
-    public void writeLiveDocs(MutableBits bits,
-                              Directory dir,
-                              SegmentInfoPerCommit info,
-                              int newDelCount,
-                              IOContext context) throws IOException {
-        BitSet set = ((FDBBits) bits).bits;
-        int size = bits.length();
-        BytesRef scratch = new BytesRef();
+    public void writeLiveDocs(MutableBits liveDocs, Directory directory, SegmentInfoPerCommit info, int newDelCount, IOContext context) {
+        neverCalled();
 
-        String fileName = IndexFileNames.fileNameFromGeneration(
-                info.info.name,
-                LIVEDOCS_EXTENSION,
-                info.getNextDelGen()
-        );
-        IndexOutput out = null;
-        boolean success = false;
-        try {
-            out = dir.createOutput(fileName, context);
-            FDBUtil.write(out, SIZE);
-            FDBUtil.write(out, Integer.toString(size), scratch);
-            FDBUtil.writeNewline(out);
+        FDBDirectory dir = FDBDirectory.unwrapFDBDirectory(directory);
+        Tuple livTuple = makeLivTuple(dir, info);
 
-            for(int i = set.nextSetBit(0); i >= 0; i = set.nextSetBit(i + 1)) {
-                FDBUtil.write(out, DOC);
-                FDBUtil.write(out, Integer.toString(i), scratch);
-                FDBUtil.writeNewline(out);
-            }
+        FDBBits bits = (FDBBits)liveDocs;
+        dir.txn.set(livTuple.pack(), Tuple.from(bits.size).pack());
 
-            FDBUtil.write(out, END);
-            FDBUtil.writeNewline(out);
-            success = true;
-        } finally {
-            if(success) {
-                IOUtils.close(out);
-            } else {
-                IOUtils.closeWhileHandlingException(out);
-            }
+        for(int i = bits.bitSet.nextSetBit(0); i >= 0; i = bits.bitSet.nextSetBit(i + 1)) {
+            dir.txn.set(livTuple.add(i).pack(), EMPTY_BYTES);
         }
     }
 
     @Override
     public void files(SegmentInfoPerCommit info, Collection<String> files) throws IOException {
-        if(info.hasDeletions()) {
-            files.add(IndexFileNames.fileNameFromGeneration(info.info.name, LIVEDOCS_EXTENSION, info.getDelGen()));
-        }
+        // TODO: Needed?
+        //if(info.hasDeletions()) {
+        //    files.add(IndexFileNames.fileNameFromGeneration(info.info.name, LIVEDOCS_EXTENSION, info.getDelGen()));
+        //}`
     }
 
-    // read-only
-    static class FDBBits implements Bits
-    {
-        final BitSet bits;
-        final int size;
 
-        FDBBits(BitSet bits, int size) {
-            this.bits = bits;
+    //
+    // Helpers
+    //
+
+    private static Tuple makeLivTuple(FDBDirectory dir, SegmentInfoPerCommit info) {
+        return dir.subspace.add(info.info.name).add(LIVEDOCS_EXTENSION).add(info.getNextDelGen());
+    }
+
+    // Until a test is found that exercises this;
+    private static void neverCalled() {
+        throw new IllegalStateException("Never called");
+    }
+
+    private static class FDBBits implements Bits, MutableBits
+    {
+        final int size;
+        final BitSet bitSet;
+
+        public FDBBits(int size) {
+            this(new BitSet(size), size);
+        }
+
+        public FDBBits(BitSet bitSet, int size) {
+            this.bitSet = bitSet;
             this.size = size;
+        }
+
+        public FDBBits(FDBBits bits) {
+            this.bitSet = (BitSet)bits.bitSet.clone();
+            this.size = bits.size;
         }
 
         @Override
         public boolean get(int index) {
-            return bits.get(index);
+            return bitSet.get(index);
         }
 
         @Override
         public int length() {
             return size;
         }
-    }
-
-    // read-write
-    static class FDBMutableBits extends FDBBits implements MutableBits
-    {
-
-        FDBMutableBits(int size) {
-            this(new BitSet(size), size);
-            bits.set(0, size);
-        }
-
-        FDBMutableBits(BitSet bits, int size) {
-            super(bits, size);
-        }
 
         @Override
         public void clear(int bit) {
-            bits.clear(bit);
+            bitSet.clear(bit);
         }
     }
 }
